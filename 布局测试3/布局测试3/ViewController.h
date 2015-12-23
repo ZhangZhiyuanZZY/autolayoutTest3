3973ecd8c4749d9e636cff5000030f15730b841d
章芝源 <610925874@qq.com>
2015-12-23 23:33:34 +0800

    完成1

---
+++ 布局测试3/布局测试3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  布局测试3
+//
+//  Created by 章芝源 on 15/12/23.
+//  Copyright © 2015年 ZZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 布局测试3/布局测试3/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  布局测试3
+//
+//  Created by 章芝源 on 15/12/23.
+//  Copyright © 2015年 ZZY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
